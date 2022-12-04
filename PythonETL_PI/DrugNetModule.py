bef289b292b41c9a3774268152b043cf3e16fbbb
rafaeldr <41450571+rafaeldr@users.noreply.github.com>
2022-12-03 23:53:25 -0300

    Simplificação no Módulo de Análise de Rede

--- PythonETL_PI/DrugNetModule.py
+++ PythonETL_PI/DrugNetModule.py
@@ -1,15 +1,27 @@
 import pandas as pd
-#import cairocffi
 import igraph as ig
-import zipfile
+import networkx as nx
 
-zip_edges = r"..\DataSources\edges.zip"
+# In
+exp_csv_interactions = r"..\Exported\exp_csv_interactions.csv"
+exp_csv_KEGG_Interaction = r"..\Exported\exp_csv_KEGG_Interaction.csv"
 
-with zipfile.ZipFile(zip_edges) as z:
-    with z.open("edges.csv") as f:
-        net_edges = pd.read_csv(f, header=0)
-        drug_graph = ig.Graph.DataFrame(net_edges, directed=False)
+# Out
+exp_graph_DrugBank = r"..\Exported\Analysis\exp_graph_DrugBank.net"
+exp_graph_KEGGDrug = r"..\Exported\Analysis\exp_graph_KEGGDrug.net"
+exp_graph_DrugBank_dot = r"..\Exported\Analysis\exp_graph_DrugBank.dot"
+exp_graph_KEGGDrug_dot = r"..\Exported\Analysis\exp_graph_KEGGDrug.dot"
 
-ig.plot(drug_graph)
+# Load DataFrames
+df_DrugBank_edges = pd.read_csv(exp_csv_interactions, sep=',')
+df_KEGGDrug_edges = pd.read_csv(exp_csv_KEGG_Interaction, sep=',')
 
-pass
+graph_DrugBank = ig.Graph.DataFrame(df_DrugBank_edges)
+graph_DrugBank.write_pajek(exp_graph_DrugBank)
+graph_DrugBank.write_dot(exp_graph_DrugBank_dot)
+
+graph_KEGGDrug = ig.Graph.DataFrame(df_KEGGDrug_edges)
+graph_KEGGDrug.write_pajek(exp_graph_KEGGDrug)
+graph_KEGGDrug.write_dot(exp_graph_KEGGDrug_dot)
+
+#ig.plot(graph_DrugBank)
